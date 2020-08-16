59af5ffbcb3c0fdd792bed7586a97f4d1e66b7fc
seungbin <tanja1206@naver.com>
2020-08-16 20:39:42 +0900

    main

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response, NextFunction, Application } from "express";
+import { sequelize } from "./config/config";
 import morgan from "morgan";
 
 const app: Application = express();
 
 app.use(morgan("dev"));
+
+sequelize.sync();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
