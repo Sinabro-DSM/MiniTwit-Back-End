3c6771668d48c586e372fcf56659fbde8de0283c
yangseungbin <tanja1206@naver.com>
2020-08-19 21:19:54 +0900

    timeline model

---
+++ src/models/timeline.ts
@@ -0,0 +1,33 @@
+import { sequelize } from "../config/config";
+import Sequelize, { Model } from "sequelize";
+import { Image } from "./image";
+
+export class Timeline extends Model {
+  content: string;
+  userId: string;
+}
+
+Timeline.init(
+  {
+    id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    content: {
+      type: Sequelize.STRING,
+    },
+    userId: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Timeline",
+  }
+);
+
+Timeline.hasMany(Image, { foreignKey: "timelineId", sourceKey: "id" });
+Image.belongsTo(Timeline, { foreignKey: "timelineId " });
