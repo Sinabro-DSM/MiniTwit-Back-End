0631e46c7320fb185b3bcde6df9caa418220085d
yangseungbin <tanja1206@naver.com>
2020-08-19 21:19:25 +0900

    db config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 import path from "path";
-dotenv.config({ path: path.join(__dirname + "../../.env") });
+dotenv.config({ path: path.join(__dirname + "../../../.env") });
 
 export const config = {
   test_module: false,
