e09909e2767008a28e08a24728c55780cb0a7b25
yangseungbin <tanja1206@naver.com>
2020-08-21 11:07:55 +0900

    make token

---
+++ src/controller/user/mkToken.ts
@@ -0,0 +1,37 @@
+import { Request } from "express";
+import jwt from "jsonwebtoken";
+
+export const mkAccess = async (req: Request, user: object): Promise<string> => {
+  const secret = req.app.get("jwt-secret");
+  const token: string = await jwt.sign(
+    {
+      id: user["id"],
+      email: user["email"],
+      nickname: user["nickname"],
+    },
+    secret,
+    {
+      expiresIn: "30m",
+    }
+  );
+  return token;
+};
+
+export const mkRefresh = async (
+  req: Request,
+  user: object
+): Promise<string> => {
+  const secret = req.app.get("refresh-secret");
+  const token: string = await jwt.sign(
+    {
+      id: user["id"],
+      email: user["email"],
+      nickname: user["nickname"],
+    },
+    secret,
+    {
+      expiresIn: "7d",
+    }
+  );
+  return token;
+};
