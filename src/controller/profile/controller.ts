6f386c08df748018d8cbe40cccc094f8adffce71
yangseungbin <tanja1206@naver.com>
2020-08-22 00:53:47 +0900

    profile controller

---
+++ src/controller/profile/controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import * as query from "./query";
+
+export const changeProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id: string = req["decoded"].id;
+  const img: string = req.file["key"];
+  const user: any = await query.findUserById(id);
+  if (!user) throw new Error("존재하지 않는 유저");
+  user.img = img;
+  await user.save();
+  res.status(200).json({ message: "성공" });
+};
+
+export const search = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const nickname = req.body.nickname;
+  const users = await query.searchUser(nickname);
+  res.status(200).json({ message: "성공", users });
+};
