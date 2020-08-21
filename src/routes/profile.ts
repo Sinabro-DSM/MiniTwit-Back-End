a4d0e1429161fd4cdee2e33dcc024387c7e78497
yangseungbin <tanja1206@naver.com>
2020-08-22 00:53:30 +0900

    profile router

---
+++ src/routes/profile.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { tryCatchMiddleware } from "../middlewares/tryCatch";
+import { authMiddleware } from "../middlewares/auth";
+import { uploadMiddleware } from "../middlewares/upload";
+import * as Profile from "../controller/profile/controller";
+
+const router = express();
+
+router.put(
+  "/",
+  authMiddleware,
+  uploadMiddleware.single("file"),
+  tryCatchMiddleware.NotFound(Profile.changeProfile)
+);
+
+router.get("/search", tryCatchMiddleware.ServerError(Profile.search));
+
+export default router;
