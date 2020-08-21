b79ff13c698f11a671c836cae8ceeab335f02420
yangseungbin <tanja1206@naver.com>
2020-08-21 11:06:33 +0900

    main

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Request, Response, NextFunction, Application } from "express";
 import { sequelize } from "./config/config";
 import morgan from "morgan";
 import router from "./routes";
+import * as dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(__dirname + "../../.env") });
 
 const app: Application = express();
 
@@ -19,6 +22,9 @@ app.use((err, req: Request, res: Response, next: NextFunction) => {
     message: err.message,
   });
 });
+
+app.set("jwt-secret", process.env.JWT_SECRET);
+app.set("refresh-secret", process.env.REFRESH_SECRET);
 
 app.listen(3000, () => {
   console.log("server on!");
