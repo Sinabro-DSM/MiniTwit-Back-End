0832d3539c0aa20543130282b003375ba5612916
yangseungbin <tanja1206@naver.com>
2020-08-24 00:05:13 +0900

    connect router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import user from "./user";
 import profile from "./profile";
+import timeline from "./timeline";
 
 const router = express();
 
 router.use("/user", user);
 router.use("/profile", profile);
+router.use("/timeline", timeline);
 
 export default router;
