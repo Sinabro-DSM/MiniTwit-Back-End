7531eb671b93b4ee6da3c428844ff464f2bc0d65
yangseungbin <tanja1206@naver.com>
2020-08-24 21:15:46 +0900

    profile query

--- src/controller/profile/query.ts
+++ src/controller/profile/query.ts
@@ -1,7 +1,9 @@
 import { User } from "../../models/user";
-import { Op } from "sequelize";
+import { Timeline } from "../../models/timeline";
+import { Image } from "../../models/image";
+import sequelize, { Op, fn } from "sequelize";
 
-export const findUserById = async (id): Promise<User> => {
+export const findUserById = async (id: string): Promise<User> => {
   try {
     const user: any = User.findOne({ where: { id } });
     return user;
@@ -10,13 +12,44 @@ export const findUserById = async (id): Promise<User> => {
   }
 };
 
-export const searchUser = async (nickname): Promise<User> => {
+export const searchUser = async (nickname: string): Promise<User> => {
   try {
     const users: any = await User.findAll({
       attributes: ["id", "nickname", "img"],
       where: { nickname: { [Op.like]: `%${nickname}%` } },
     });
     return users;
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const showUser = async (id: string): Promise<object> => {
+  try {
+    const profile: any = await User.findOne({
+      include: [
+        {
+          model: User,
+          as: "Followings",
+          attributes: [
+            [sequelize.fn("Count", sequelize.col("FollowingId")), "followings"],
+          ],
+        },
+        {
+          model: Timeline,
+          attributes: ["id", "content"],
+          include: [
+            {
+              model: Image,
+              attributes: ["id", "img"],
+            },
+          ],
+        },
+      ],
+      attributes: ["email", "nickname", "img"],
+      where: { id },
+    });
+    return profile;
   } catch (e) {
     throw e;
   }
