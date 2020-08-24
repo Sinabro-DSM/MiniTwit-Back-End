d1a7651296de30e8c762eec10a4858bbb910ada1
yangseungbin <tanja1206@naver.com>
2020-08-25 02:05:00 +0900

    user router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,11 +4,7 @@ import * as User from "../controller/user/controller";
 
 const router = express();
 
-router.post(
-  "/email/send",
-  tryCatchMiddleware.Conflict(User.emailExist),
-  tryCatchMiddleware.ServerError(User.emailSend)
-);
+router.post("/email/send", tryCatchMiddleware.Conflict(User.emailSend));
 router.post("/register", tryCatchMiddleware.Conflict(User.signUp));
 router.post("/login", tryCatchMiddleware.NotFound(User.login));
 
