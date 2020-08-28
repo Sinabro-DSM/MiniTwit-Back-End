0c58ebc7cfa36017edd73e241235ab060fc90715
seungbin <tanja1206@naver.com>
2020-08-28 23:01:21 +0900

    profile query

--- src/controller/profile/query.ts
+++ src/controller/profile/query.ts
@@ -2,6 +2,7 @@ import { User } from "../../models/user";
 import { Timeline } from "../../models/timeline";
 import { Image } from "../../models/image";
 import { Op } from "sequelize";
+import { Like } from "../../models/like";
 
 export const findUserById = async (id: string): Promise<User> => {
   try {
@@ -46,12 +47,26 @@ export const showUser = async (id: string): Promise<object> => {
               model: Image,
               attributes: ["id", "img"],
             },
+            {
+              model: Like,
+              attributes: ["userId"],
+            },
           ],
         },
       ],
       attributes: ["email", "nickname", "img"],
       where: { id },
     });
+
+    for (let i = 0; i < profile.Timelines.length; i++) {
+      profile.Timelines[i].dataValues.isLike = false;
+      for (let j = 0; j < profile.Timelines[i].Likes.length; j++) {
+        if (profile.Timelines[i].Likes[j].userId === id) {
+          profile.Timelines[i].dataValues.isLike = true;
+        }
+      }
+    }
+    console.log(profile.Timelines);
     return profile;
   } catch (e) {
     throw e;
