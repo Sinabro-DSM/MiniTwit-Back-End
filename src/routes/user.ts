2eb719c582ccadf23342f5444bf3aeb20fddade5
seungbin <tanja1206@naver.com>
2020-08-28 23:27:38 +0900

    user router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { refreshMiddleware } from "../middlewares/auth";
 import { tryCatchMiddleware } from "../middlewares/tryCatch";
 import * as User from "../controller/user/controller";
 
@@ -7,5 +8,10 @@ const router = express();
 router.post("/email/send", tryCatchMiddleware.Conflict(User.emailSend));
 router.post("/register", tryCatchMiddleware.Conflict(User.signUp));
 router.post("/login", tryCatchMiddleware.NotFound(User.login));
+router.get(
+  "/refresh",
+  refreshMiddleware,
+  tryCatchMiddleware.NotFound(User.refresh)
+);
 
 export default router;
